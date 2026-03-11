06bc304140e94e7a9204d842e365d4a226b5d398
infinityrodeojanmp540do-cmd <infinity.rodeojanmp540do@gmail.com>
2026-03-11 10:53:18 +0900

    feat: add front-page template

---
+++ themes/naoki-theme/front-page.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+<main>
+
+<section>
+  <h1>Naoki Portfolio</h1>
+  <p>WordPress Portfolio Site</p>
+</section>
+
+<section>
+  <h2>About</h2>
+  <p>自己紹介エリア</p>
+</section>
+
+<section>
+  <h2>Works</h2>
+  <p>制作実績エリア</p>
+</section>
+
+<section>
+  <h2>Contact</h2>
+  <p>お問い合わせ</p>
+</section>
+
+</main>
+
+<?php get_footer(); ?>
